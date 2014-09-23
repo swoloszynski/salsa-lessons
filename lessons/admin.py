69869afd6c709f2c96939370ab4829d243465b97
Sam Woloszynski <sam.w@virginia.edu>
2014-09-23 10:51:19 -0400

    Separate Practice fields into fieldsets and collapse date and location

--- lessons/admin.py
+++ lessons/admin.py
@@ -2,7 +2,10 @@ from django.contrib import admin
 from lessons.models import Practice, Lesson
 
 class PracticeAdmin(admin.ModelAdmin):
-    fields = ['date', 'location', 'overview', 'lessons', 'notes']
+    fieldsets = [
+        (None,                {'fields': ['overview', 'lessons', 'notes']}),
+        ('Date and Location', {'fields': ['date', 'location'], 'classes': ['collapse']}),
+    ]
 
 admin.site.register(Practice, PracticeAdmin)
 admin.site.register(Lesson)
