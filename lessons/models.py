3520f85599f7595c0a76f02c35c402e05f038c36
Sam Woloszynski <sam.w@virginia.edu>
2014-09-24 18:04:15 -0400

    Restore Teaches (through) model and allow notes fields to be blank

--- lessons/models.py
+++ lessons/models.py
@@ -12,8 +12,13 @@ class Practice(models.Model):
     date = models.DateTimeField()
     location = models.CharField(max_length=200)
     overview = models.CharField(max_length=200)
-    notes = models.CharField(max_length=700)
-    lessons = models.ManyToManyField(Lesson)
+    notes = models.CharField(max_length=700, blank=True, null=True)
+    teachings = models.ManyToManyField(Lesson, through='Teaches', through_fields=('practice', 'lesson'))
     def __str__(self):
         date_string = self.date.strftime("%b %d, %Y")
         return date_string + " - " + self.overview
+
+class Teaches(models.Model):
+    practice = models.ForeignKey(Practice)
+    lesson = models.ForeignKey(Lesson)
+    notes = models.CharField(max_length=200, blank=True, null=True)
