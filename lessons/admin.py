2706c4313cb767736061b1c8cad0fcfe08315f2b
Sam Woloszynski <sam.w@virginia.edu>
2014-09-24 17:58:03 -0400

    Restore migration, use many to many through relationship and inlines

--- lessons/admin.py
+++ lessons/admin.py
@@ -1,6 +1,21 @@
 from django.contrib import admin
 from lessons.models import Practice, Lesson, Teaches
 
-admin.site.register(Practice)
+class TeachingsInline(admin.TabularInline):
+    model = Practice.teachings.through
+    extra = 1
+
+class LessonAdmin(admin.ModelAdmin):
+    inlines = [
+        TeachingsInline,
+    ]
+
+class PracticeAdmin(admin.ModelAdmin):
+    inlines = [
+        TeachingsInline,
+    ]
+    exclude = ('lessons',)
+
+
+admin.site.register(Practice, PracticeAdmin)
 admin.site.register(Lesson)
-admin.site.register(Teaches)
