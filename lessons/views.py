be878553b43be9daf72ee85dfd2495978d41fe93
Sam Woloszynski <sam.w@virginia.edu>
2014-10-01 12:07:34 -0400

    Use render as shortcut to using template

--- lessons/views.py
+++ lessons/views.py
@@ -1,12 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.template import RequestContext, loader
 from lessons.models import Practice, Lesson
 
 def index(request):
     upcoming_practice_list = Practice.objects.all() #order_by('-date')[:5]
-    template = loader.get_template('lessons/index.html')
-    context = RequestContext(request, {
-        'upcoming_practice_list': upcoming_practice_list,
-    })
-    return HttpResponse(template.render(context))
+    context = {'upcoming_practice_list': upcoming_practice_list}
+    return render(request, 'lessons/index.html', context)
