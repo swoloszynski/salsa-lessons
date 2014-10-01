53e796be4199cbcff13481e3bf5943570bf492a9
Sam Woloszynski <sam.w@virginia.edu>
2014-10-01 12:28:42 -0400

    Replace hardcoded urls with url tag and add practice detail view

--- lessons/urls.py
+++ lessons/urls.py
@@ -4,4 +4,6 @@ from lessons import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
+    # ex: /lessons/5/
+    url(r'^(?P<practice_id>\d+)/$', views.practice_detail, name='practice_detail'),
 )
--- lessons/views.py
+++ lessons/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 from lessons.models import Practice, Lesson
 
@@ -6,3 +6,8 @@ def index(request):
     upcoming_practice_list = Practice.objects.all() #order_by('-date')[:5]
     context = {'upcoming_practice_list': upcoming_practice_list}
     return render(request, 'lessons/index.html', context)
+
+def practice_detail(request, practice_id):
+    practice = get_object_or_404(Practice, pk=practice_id)
+    context = {'practice': practice}
+    return render(request, 'lessons/practice_detail.html', context)
