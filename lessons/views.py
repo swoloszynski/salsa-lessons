60ecb263b3c14cf317b61d759db9d185a48eda10
Sam Woloszynski <sam.w@virginia.edu>
2014-10-01 11:48:23 -0400

    Add template for index

--- lessons/views.py
+++ lessons/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import RequestContext, loader
 from lessons.models import Practice, Lesson
 
 def index(request):
     upcoming_practice_list = Practice.objects.all() #order_by('-date')[:5]
-    output = ', '.join([p.overview for p in upcoming_practice_list])
-    return HttpResponse(output)
+    template = loader.get_template('lessons/index.html')
+    context = RequestContext(request, {
+        'upcoming_practice_list': upcoming_practice_list,
+    })
+    return HttpResponse(template.render(context))
