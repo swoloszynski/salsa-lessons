c1d670e10f695efca38fbcc71928276b055a9520
Sam Woloszynski <sam.w@virginia.edu>
2014-10-30 03:17:53 -0400

    In practice detail make lesson content show/hide on click

---
+++ lessons/static/lessons/javascript.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+  $('.hidden').hide();
+
+
+  $('.lesson-title').click(function(){
+    arrownDownPath = '/static/lessons/images/arrowDown.png';
+    arrownRightPath = '/static/lessons/images/arrowRight.png';
+
+    content = $(this).parent().find('.lesson-content-container');
+    $content = $(content);
+    isDisplayed = $content.hasClass('displayed');
+    arrow = $(this).find('.arrow');
+    $arrow = $(arrow);
+
+    if(isDisplayed) {
+      $content.removeClass('displayed');
+      $content.addClass('hidden');
+      $content.hide();
+      $arrow.attr('src', arrownRightPath);
+      $arrow.removeClass('arrow-down');
+      $arrow.addClass('arrow-right');
+    }
+    else {
+      $content.removeClass('hidden');
+      $content.addClass('displayed');
+      $content.show();
+      $arrow.attr('src', arrownDownPath);
+      $arrow.removeClass('arrow-right');
+      $arrow.addClass('arrow-down');
+    }
+
+  });
+});
